646dcb58853b017f487a3e6b5b855ffc9816e927
aarongerig <aaron.gerig@instride.ch>
2024-08-20 15:25:19 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,152 @@
+$(document).ready(function() {
+
+  'use strict';
+
+  $('body').addClass('js-enabled');
+
+  // =====================
+  // Koenig Gallery
+  // =====================
+  var gallery_images = document.querySelectorAll('.kg-gallery-image img');
+
+  gallery_images.forEach(function (image) {
+    var container = image.closest('.kg-gallery-image');
+    var width = image.attributes.width.value;
+    var height = image.attributes.height.value;
+    var ratio = width / height;
+    container.style.flex = ratio + ' 1 0%';
+  });
+
+  // =====================
+  // Decode HTML entities returned by Ghost translations
+  // Input: Plus d&#x27;articles
+  // Output: Plus d'articles
+  // =====================
+
+  function decoding_translation_chars(string) {
+    return $('<textarea/>').html(string).text();
+  }
+
+  // =====================
+  // Responsive layout
+  // =====================
+
+  // Init Masonry
+  var $masonry_grid = $('.js-grid').masonry({
+    itemSelector: '.js-grid__col',
+    percentPosition: true
+  });
+
+  // Layout Masonry after each image loads
+  $masonry_grid.imagesLoaded().progress(function() {
+    $masonry_grid.masonry('layout');
+  });
+
+  // =====================
+  // Off Canvas menu
+  // =====================
+
+  $('.js-off-canvas-toggle').click(function(e) {
+    e.preventDefault();
+    $('.js-off-canvas-content, .js-off-canvas-container').toggleClass('is-active');
+  });
+
+  // =====================
+  // Post Card Images Fade
+  // =====================
+
+  $('.js-fadein').viewportChecker({
+    classToAdd: 'is-inview', // Class to add to the elements when they are visible
+    offset: 100,
+    removeClassAfterAnimation: true
+  });
+
+  // =====================
+  // Responsive videos
+  // =====================
+
+  $('.c-content').fitVids({
+    'customSelector': [ 'iframe[src*="ted.com"]'          ,
+                        'iframe[src*="player.twitch.tv"]' ,
+                        'iframe[src*="dailymotion.com"]'  ,
+                        'iframe[src*="facebook.com"]'
+                      ]
+  });
+
+  // =====================
+  // Images zoom
+  // =====================
+
+  $('.c-post img').attr('data-action', 'zoom');
+
+  // If the image is inside a link, remove zoom
+  $('.c-post a img').removeAttr('data-action');
+
+  // =====================
+  // Clipboard URL Copy
+  // =====================
+
+  var clipboard = new ClipboardJS('.js-share__link--clipboard');
+
+  clipboard.on('success', function(e) {
+    var element = $(e.trigger);
+
+    element.addClass('tooltipped tooltipped-s');
+    element.attr('aria-label', clipboard_copied_text);
+
+    element.mouseleave(function() {
+      $(this).removeAttr('aria-label');
+      $(this).removeClass('tooltipped tooltipped-s');
+    });
+  });
+
+  // =====================
+  // Responsive tables
+  // =====================
+
+  $('.c-content table').wrap("<div class='responsive-table'></div>");
+
+  // =====================
+  // Ajax Load More
+  // =====================
+
+  var pagination_next_url = $('link[rel=next]').attr('href'),
+    $load_posts_button = $('.js-load-posts');
+
+  $load_posts_button.click(function(e) {
+    e.preventDefault();
+
+    var request_next_link =
+      pagination_next_url.split(/page/)[0] +
+      'page/' +
+      pagination_next_page_number +
+      '/';
+
+    $.ajax({
+      url: request_next_link,
+      beforeSend: function() {
+        $load_posts_button.text(decoding_translation_chars(pagination_loading_text));
+        $load_posts_button.addClass('c-btn--loading');
+      }
+    }).done(function(data) {
+      var posts = $('.js-post-card-wrap', data);
+
+      $('.js-grid').append(posts).masonry('appended', posts);
+
+      $masonry_grid.imagesLoaded().progress(function() {
+        $masonry_grid.masonry('layout');
+        $('.js-fadein').addClass('is-inview');
+      });
+
+      $load_posts_button.text(decoding_translation_chars(pagination_more_posts_text));
+      $load_posts_button.removeClass('c-btn--loading');
+
+      pagination_next_page_number++;
+
+      // If you are on the last pagination page, hide the load more button
+      if (pagination_next_page_number > pagination_available_pages_number) {
+        $load_posts_button.addClass('c-btn--disabled').attr('disabled', true);
+      }
+    });
+  });
+});
---
+++ generate_release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r ~/Desktop/sinai.zip . -x "generate_release.sh" ".idea/*"
